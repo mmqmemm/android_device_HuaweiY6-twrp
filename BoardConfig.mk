b8a4c6d9d6639ff1c55dd2d0d1dab9aa38b560e9
mmqmemm <rdgbnirr3418435@163.com>
2024-08-16 20:13:28 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,7 +50,7 @@ TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_PAGESIZE    := 2048
-BOARD_KERNEL_SEPARATED_DT := true
+# BOARD_KERNEL_SEPARATED_DT := true
 TARGET_PREBUILT_KERNEL := device/huawei/scale/prebuilt/kernel
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 --dt $(LOCAL_PATH)/prebuilt/dt.img
 
