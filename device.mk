d599c23b0dbad2566caaaa9484b98aa80ce4dc52
mmqmemm <rdgbnirr3418435@163.com>
2024-08-16 18:45:24 +0800

    Delete device.mk

--- device.mk
+++
@@ -1,8 +0,0 @@
-# Path to overlay files
-DEVICE_PACKAGE_OVERLAYS += device/huawei/scale/overlay
-
-# Files needed for recovery image
-PRODUCT_COPY_FILES += \
-device/huawei/scale/prebuilt/kernel:kernel \
-device/huawei/scale/prebuilt/dt.img:dt.img 
-
